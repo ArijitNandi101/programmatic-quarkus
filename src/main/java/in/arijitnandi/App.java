68b99f0adf09d2e22e66192cf8a6c01165db84b1
ArijitNandi101 <arijitn001@gmail.com>
2023-06-27 14:39:10 +0530

    task: add quarkus dependencies and bom

--- src/main/java/in/arijitnandi/App.java
+++ src/main/java/in/arijitnandi/App.java
@@ -1,13 +1,14 @@
 package in.arijitnandi;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import io.quarkus.runtime.Quarkus;
+
+public class App
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        Quarkus.run(CLIQuarkusApplication.class, new String[0]);
+        Quarkus.blockingExit();
+
+        System.exit(0);
     }
 }
